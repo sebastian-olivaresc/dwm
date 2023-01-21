0dd389c557eec219c79e6cf45aaf46f561baf271
DrZaius <sebastian.olivaresc@sansano.usm.cl>
2023-01-21 16:30:14 -0300

    first commit

--- config.h
+++ config.h
@@ -1,30 +1,47 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
+
+// pywal colors
+#include "/home/drzaius/.cache/wal/colors-wal-dwm.h"
+
+#define XF86MonBrightnessUp 0x1008ff02
+#define XF86MonBrightnessDown 0x1008ff03 
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 45;        /* gaps between windows */
+static const unsigned int gappx     = 25;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
+static const int usealtbar          = 0;        /* 1 means use non-dwm status bar */
 static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
 static const char *alttrayname      = "tray";    /* Polybar tray instance name */
 static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { 
+    "hack :pixelsize= 30",
+    "Symbols Nerd Fonts:pixelsize=40:antialias=true;autohint=true", };
 static const char dmenufont[]       = "monospace:size=10";
+
+
+
+/*
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
+*/
 	/*               fg         bg         border   */
+/*
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
+*/
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "󰎤", "󰎧", "󰎪", "󰎭", "󰎱", "󰎳", "󰎶", "󰎹", "󰎼"};
 static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
@@ -50,6 +67,16 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 };
 
+/* Fn-keys functions */
+static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "3", "+10%",     NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "3", "-10%",     NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "3", "toggle",  NULL };
+
+static const char *light_up[] = {"/usr/local/bin/light", "-A", "10", NULL};
+static const char *light_down[] = {"/usr/local/bin/light", "-U", "10", NULL};
+static const char *kb_light[] = {"/usr/local/bin/light", "-Srs",
+	"sysfs/leds/platform::kbd_backlight", "1", NULL};
+
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
@@ -63,16 +90,16 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *roficmd[] = { "rofi", "-show","drun",  NULL};
-static const char *termcmd[]  = { "st", NULL };
-static const char *betterlockscreen[]  = { "betterlockscreen", "-l" , "dim",  NULL };
+//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *roficmd[] = { "rofi", "-show", "drun", NULL};
+static const char *termcmd[]  = { "kitty", NULL };
+static const char *betterlockscreen[]  = { "betterlockscreen", "-l", "dim",  NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_x,      spawn,          {.v = betterlockscreen} },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = betterlockscreen} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_b,      hideborder,     {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -97,8 +124,6 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_n,      togglealttag,   {1} },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -109,6 +134,12 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ 0,          XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
+	{ 0,          XF86XK_AudioMute, spawn, {.v = mutevol } },
+	{ 0,          XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
+  { 0,					XF86MonBrightnessUp,		spawn,	{.v = light_up} },
+	{ 0,					XF86MonBrightnessDown,	spawn,	{.v = light_down} },
+	{ 0,					0xffeb,	spawn,	{.v = kb_light} },
 };
 
 /* button definitions */
@@ -126,4 +157,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
